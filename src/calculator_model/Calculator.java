55662b944b25770e9af550a1deb58d875596141f
Brendan Tiu <brendan.tiu@mail.utoronto.ca>
2019-12-28 01:19:54 -0500

    Implement toString method

--- src/calculator_model/Calculator.java
+++ src/calculator_model/Calculator.java
@@ -3,25 +3,29 @@ package calculator_model;
 import util.Observable;
 
 public class Calculator extends Observable {
-	public int number;
+	public double number;
 	
-	public void add(int n) {
+	public void add(double n) {
 		this.number += n;
 		this.notifyObservers();
 	}
 	
-	public void subtract(int n) {
+	public void subtract(double n) {
 		this.number -= n;
 		this.notifyObservers();
 	}
 	
-	public void divide(int n) {
+	public void divide(double n) {
 		this.number /= n;
 		this.notifyObservers();
 	}
 	
-	public void multiply(int n) {
+	public void multiply(double n) {
 		this.number *= n;
 		this.notifyObservers();
+	}
+	
+	public String toString() {
+		return String.valueOf(this.number);
 	}
 }
